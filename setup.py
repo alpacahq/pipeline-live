799e37829cd3d5d78563a2a295f18290a7537379
Shlomi Kushchi <shlomik@gmail.com>
2021-01-11 11:10:49 +0200

    ci:: limit pandas-datareader<=0.8.1 to work with other packages.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'pandas==0.22.0',
         'numpy==1.19.4',
         'scipy<=1.6.0',
+        'pandas-datareader<=0.8.1',
         'alpaca-trade-api>=0.51.0',
         'iexfinance>=0.4.1,<0.5.0',
         'zipline==1.3.0',
