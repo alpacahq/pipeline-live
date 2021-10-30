db67917cd9955f58854b3202bb7a21f8cdb30c9e
camelpac <78740792+camelpac@users.noreply.github.com>
2021-10-30 10:38:59 +0300

    upgrade pipeline-live to python > 3.7

--- setup.py
+++ setup.py
@@ -25,14 +25,9 @@ setup(
     keywords='financial,zipline,pipeline,stock,screening,api,trade',
     packages=find_packages(),
     install_requires=[
-        'pandas==0.22.0',
-        'numpy==1.19.4',
-        'scipy<=1.6.0',
-        'pandas-datareader<=0.8.1',
-        'lxml>=4.6.2',
-        'alpaca-trade-api>=0.52.0',
-        'iexfinance>=0.4.1,<0.5.0',
-        'zipline==1.3.0',
+        'alpaca-trade-api>=1.4.0',
+        'iexfinanc',
+        'zipline-reloaded',
     ],
     tests_require=[
         'pytest',
